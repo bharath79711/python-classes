6427e49330b084e84356bc46a7c78f908c9cf6d7
Bharath79711 <bharathkumartalari70@gmail.com>
2025-07-23 12:36:20 +0530

    modules and logging

---
+++ class_19.py
@@ -0,0 +1,64 @@
+
+
+'''
+from module.regular_expression import *
+z=add(10,4)
+print(z)
+
+xx=subt(10,4)
+print(xx)
+
+yy=product(10,4)
+print(yy)
+
+'''
+'''
+import regular_expression as r
+
+z=r.add(10,4)
+print(z)
+
+xx=r.subt(10,4)
+print(xx)
+
+yy=r.product(10,4)
+print(yy)
+
+
+'''
+'''
+from module import regular_expression
+
+z=regular_expression.add(10,4)
+print(z)
+
+xx=regular_expression.subt(10,4)
+print(xx)
+
+yy=regular_expression.product(10,4)
+print(yy)
+'''
+'''
+import logging
+logging.basicConfig(filename="loger.txt",level=logging.DEBUG)
+logging.debug("This is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
+
+
+
+
+
+'''
+import logging
+logging.basicConfig(filename="loger.txt",level=logging.ERROR)
+logging.debug("This is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
+
+
+
